546d6f96921c7d00a419c9cd27524517e5c7ef54
Muffin <marvin.dick96@web.de>
2018-05-19 22:26:04 +0200

    fixed reading from json #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  private concertsUrl = './assets/concerts.json';
+
+  concerts : string [];
+
+  constructor (private httpService: HttpClient) {}
+
+  ngOnInit() {
+    this.httpService.get(this.concertsUrl)
+      .subscribe(
+      data => {
+        this.concerts = data["concerts"] as string [];
+          console.log(this.concerts[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+
+  }
+
 }
